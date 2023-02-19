5131a1d501467cc11459e99a2a2904f920f8fd5d
Игорь Михайлович Панасеня <panasenya1995@mail.ru>
2023-02-19 19:15:08 +0300

    fix

--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import ENV from '../config.js';
 
 async function connect() {
-    const mongod = await MongoMemoryServer.create();
+    // const mongod = await MongoMemoryServer.create();
     // const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true);
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/connect.js';
 import router from './router/route.js';
+import mongoose from 'mongoose';
 
 const app = express();
+const CONNECTION_URL = "mongodb+srv://admin:wwwwww@cluster0.t5aoutx.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.json());
 app.use(cors());
@@ -21,16 +23,30 @@ app.get('/', (req, res) => {
 // API routes
 app.use('/api', router);
 
-
-// start server only when we have valid connection
-connect().then(() => {
-    try {
-        app.listen(process.env.PORT || port, () => {
-            console.log(`Server connected to http://localhost:${port}`)
-        })
-    } catch (error) {
-        console.log('cannot connect to the server')
-    }
-}).catch(error => {
+mongoose.connect(CONNECTION_URL)
+    .then(() => {
+        try {
+            app.listen(process.env.PORT || port, () => {
+                console.log(`Server connected to http://localhost:${port}`)
+            })
+        } catch (error) {
+            console.log('cannot connect to the server')
+        }
+    }).catch(error => {
     console.log('ERROR Database:', error)
 })
+
+
+
+// start server only when we have valid connection
+// connect().then(() => {
+//     try {
+//         app.listen(process.env.PORT || port, () => {
+//             console.log(`Server connected to http://localhost:${port}`)
+//         })
+//     } catch (error) {
+//         console.log('cannot connect to the server')
+//     }
+// }).catch(error => {
+//     console.log('ERROR Database:', error)
+// })
